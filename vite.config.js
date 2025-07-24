f75ec539ecfdd58e4bf23a2845bd3ef50bc8ed81
dropofsino <dropofsino@gmail.com>
2025-07-25 02:04:37 +0800

    尝试修改接打电话

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,24 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({ resolvers: [ElementPlusResolver()] }),
     Components({ resolvers: [ElementPlusResolver()] }),
-    mkcert(),
+    // mkcert(),
   ],
-  base: "http://localhost:8000/",
-  build: {
-    outDir: "static",
-    assetsDir: "assets",
-  },
+  // base: "http://localhost:8000/",
+  // build: {
+  //   outDir: "static",
+  //   assetsDir: "assets",
+  // },
   assetsInclude: ["**/*.wasm"],
   resolve: {
     conditions: ["wasm"],
     dedupe: ["matrix-js-sdk"],
   },
   optimizeDeps: { include: ["matrix-js-sdk"] },
-  server: { https: true },
+  // server: { https: true },
 });
