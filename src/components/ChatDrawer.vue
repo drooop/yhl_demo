62a6b2e0dfecc14438d8147bb36092d72ad590b8
Kevin_Yang <39990043+drooop@users.noreply.github.com>
2025-07-31 06:02:47 +0800

    Show chat drawer with full-screen chat

--- src/components/ChatDrawer.vue
+++ src/components/ChatDrawer.vue
@@ -1,38 +1,23 @@
 <template>
   <el-drawer
     v-model="modelValue"
-    title="房间信息"
-    size="300"
+    :with-header="false"
+    size="100%"
     direction="rtl"
-    :with-header="true"
   >
-    <template v-if="room">
-      <p><strong>ID:</strong> {{ room.roomId }}</p>
-      <p><strong>成员:</strong></p>
-      <el-scrollbar v-if="members.length" class="member-scroll">
-        <ul class="member-list">
-          <li v-for="m in members" :key="m.userId" class="member-item">
-            {{ m.name || m.userId }}
-          </li>
-        </ul>
-      </el-scrollbar>
-      <el-empty v-else description="暂无成员" />
-    </template>
-    <el-empty v-else description="请选择房间" />
+    <ChatView />
   </el-drawer>
 </template>
 
 <script setup>
 import { computed } from "vue";
-import { useRoomStore } from "@/store/rooms";
+import ChatView from "./ChatView.vue";
 
 const props = defineProps({
   modelValue: Boolean,
-  room: Object,
 });
 const emit = defineEmits(["update:modelValue"]);
 
-/* 双向绑定 */
 const modelValue = computed({
   get() {
     return props.modelValue;
@@ -41,22 +26,7 @@ const modelValue = computed({
     emit("update:modelValue", v);
   },
 });
-
-const members = computed(() =>
-  props.room ? Array.from(props.room.currentState.members.values()) : []
-);
 </script>
 
 <style scoped>
-.member-scroll {
-  max-height: 200px;
-}
-.member-list {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-.member-item {
-  padding: 4px 0;
-}
 </style>
